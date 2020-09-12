322c25a7ea1fbba5f79398d9f8c31070e789fd8c
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-09-12 17:17:29 +0200

    Started creating basic Vue Frontend
    Vue Frontend:
    - Removed some of the boilerplate code and added custom elements instead
    - Added an app bar in the App component with generated menu buttons,
    that serve as links to the custom routes
    - So far only one experimental route implemented as the Home component,
    that contains a child HelloWorld component to try out passing of prop data

--- vue-fe/src/App.vue
+++ vue-fe/src/App.vue
@@ -1,56 +1,53 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app dark>
+        {{title}}       
+        <v-spacer>
+        </v-spacer>
+        <v-btn
+          v-for="(entry,i) in menuEntries"
+          :key=i
+          :to="entry.route"
+          depressed
+          class="mx-2"
+        >
+          <v-icon
+          class="mr-2">
+            {{entry.icon}}
+          </v-icon>
+          {{entry.title}}
+        </v-btn>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld />
+         <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
-    //
+    title:"WebApp",
+    menuEntries: [
+      {
+        route: "/home",
+        icon:"mdi-home",
+        title:"Home"
+      },
+      {
+        route: "/login",
+        icon:"mdi-account",
+        title:"Login"
+      },
+      
+       {
+        route: "/logout",
+        icon:"mdi-logout",
+        title:"Logout"
+      },
+    ]
+
   })
 };
 </script>
--- vue-fe/src/views/Home.vue
+++ vue-fe/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="auto">
+        <img alt="Vue logo" src="@/assets/logo.png" />
+        <HelloWorld :name="user" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld
-  }
+  },
+  data: () => ({
+    user: "User"
+  })
 };
 </script>
