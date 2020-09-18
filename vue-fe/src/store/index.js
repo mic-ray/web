8fc2fe4adc56fecac8f66f984acf2dcb88e02af0
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-09-18 22:31:53 +0200

    Vue FE: Added basic Vuex functionality
    Vue Frontend:
    - Added some basic functionality to the Vuex store, to test it out
    - Action communicates with Node API to get some dummy data using axios
    - Action method is called on mounted() hook of Home component
    - The action method commits a mutation to set new dummy data on the state
    - State holds the current data, which is accessed by the Home component
    by using the provided getter from the store

--- vue-fe/src/store/index.js
+++ vue-fe/src/store/index.js
@@ -1,11 +1,33 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
+axios.defaults.baseURL = "http://localhost:5000/";
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    info: "Default info!"
+  },
+  mutations: {
+    setInfo: (state, data) => {
+      state.info = data;
+    }
+  },
+  getters: {
+    getInfo: state => state.info
+  },
+  actions: {
+    getInfo(state) {
+      axios
+        .get("/info")
+        .then(response => {
+          state.commit("setInfo", response.data.info);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
   modules: {}
 });
--- vue-fe/src/views/Home.vue
+++ vue-fe/src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <HelloWorld :name="user" />
+  <div>
+    <v-row><HelloWorld :name="user"/></v-row>
+    <v-row justify="center"
+      ><v-btn @click="getInfo">{{ info }}</v-btn></v-row
+    >
+  </div>
 </template>
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
@@ -9,8 +14,19 @@ export default {
   components: {
     HelloWorld
   },
+  methods: {
+    // Set info data to the value from the store
+    getInfo() {
+      this.info = this.$store.getters.getInfo;
+    }
+  },
   data: () => ({
-    user: "User"
-  })
+    user: "User",
+    info: null
+  }),
+  // Call the store action when component is mounted
+  mounted() {
+    this.$store.dispatch("getInfo");
+  }
 };
 </script>
