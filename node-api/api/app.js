e0d02771362d8e8a6e3a08be98aa1ed6459176bb
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-09-18 22:29:39 +0200

    Node API: Added CORS handling
    API:
    - Added middleware, that sets headers on all requests to handle CORS errors
    - Changed running port for testing purposes

--- node-api/api/app.js
+++ node-api/api/app.js
@@ -1,10 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { infoRoutes } = require('./routes/info');
-app.use('/info', infoRoutes);
+const { infoRoutes } = require("./routes/info");
+
+// Setup CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+app.use("/info", infoRoutes);
 app.use((req, res) => {
-    return res.status(404).send('<h1>Nothing found<h1>');
+  return res.status(404).send("<h1>Nothing found<h1>");
 });
 
 module.exports = app;
--- node-api/index.js
+++ node-api/index.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const app = require('./api/app');
+const http = require("http");
+const app = require("./api/app");
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.listen(5000);
