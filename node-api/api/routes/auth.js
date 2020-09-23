21692e1e78c454e558ad8c8378c3bb55a0219162
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-09-23 23:46:42 +0200

    Node API: Renamed file to fit other naming

--- node-api/api/routes/auth.js
+++ node-api/api/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const AuthController = require("../controllers/AuthController");
+const AuthController = require("../controllers/auth.controller");
 
 router.post("/login", AuthController.auth_login);
 
