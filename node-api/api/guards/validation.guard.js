041d5d5bec426b251ce56560a073ce72788df10d
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-09-29 20:36:24 +0200

    Node API: Reworked server side validation
    Validation guard:
    - Reworked the response in case of validation errors to include specific
    information, on why which field has failed validation
    - That way clients can use the server side validation errors to display
    them in the frontend
    
    Validation service:
    - Adjusted schemas, so that schema for the email can be used seperately,
    without the password field
    - Added custom error messages which can be returned to the client,
    depending on the failed validation
    - Set the property abortEarly to false, so that all failed fields and
    their corresponding errors are returned

--- node-api/api/guards/validation.guard.js
+++ node-api/api/guards/validation.guard.js
@@ -7,7 +7,14 @@ exports.validateCredentials = (req, res, next) => {
     password: req.body.password
   });
   if (validationError) {
-    return next(new ApiError(validationError, 400));
+    var errors = {};
+    validationError.details.forEach(err => {
+      errors[err.path[0]] = [err.message];
+    });
+    return res.status(400).json({
+      result: "Invalid data format!",
+      dataErrors: errors
+    });
   }
   next();
 };
@@ -15,7 +22,14 @@ exports.validateCredentials = (req, res, next) => {
 exports.validateEmail = (req, res, next) => {
   const validationError = ValidationService.validateEmail(req.params.email);
   if (validationError) {
-    return next(new ApiError(validationError, 400));
+    var errors = {};
+    validationError.details.forEach(err => {
+      errors[err.path[0]] = [err.message];
+    });
+    return res.status(400).json({
+      result: "Invalid data format!",
+      dataErrors: errors
+    });
   }
   next();
 };
--- node-api/api/services/validation.service.js
+++ node-api/api/services/validation.service.js
@@ -1,17 +1,31 @@
 const Joi = require("joi");
+const UsersService = require("../services/users.service");
 
-const credentialsSchema = Joi.object({
-  email: Joi.string().email().required(),
+const emailSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    "string.base": `E-Mail should be of type text`,
+    "string.empty": `E-Mail can not be empty`,
+    "string.email": `E-Mail is not valid`,
+    "any.required": `E-Mail is required`,
+    "any.external": `E-Mail is already taken`
+  })
+});
 
-  password: Joi.string().min(8).required()
+const credentialsSchema = emailSchema.append({
+  password: Joi.string().min(8).required().messages({
+    "string.base": `Password should be of type text`,
+    "string.empty": `Password can not be empty`,
+    "string.min": `Password has to be at least {#limit} characters long`,
+    "any.required": `Password is required`
+  })
 });
 
 exports.validateCredentials = credentials =>
   credentialsSchema.validate(credentials).error
-    ? credentialsSchema.validate(credentials).error.message
+    ? credentialsSchema.validate(credentials, { abortEarly: false }).error
     : null;
 
 exports.validateEmail = email =>
-  Joi.string().email().required().validate(email).error
-    ? Joi.string().email().required().validate(email).error.message
+  emailSchema.validate({ email }).error
+    ? emailSchema.validate({ email }, { abortEarly: false }).error
     : null;
