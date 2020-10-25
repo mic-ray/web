b201aa4b405ccd9eb1c31e5683df6fac6dff0d36
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-10-25 22:34:08 +0100

    Vue FE: Added action and dialog mode to edit notes
    Edit notes:
    - Added new action in data table to open edit menu for notes
    - Edit dialog reuses the create new note dialog, but it changes the
    dialog title and already sets the previously added note title and
    description, therefore sets editMode to true on dialog object
    - Saving of note edit changes to be implemented
    
    Style variables:
    - Adjusted styles file to add custom font to toast notifications

--- vue-fe/src/views/Home.vue
+++ vue-fe/src/views/Home.vue
@@ -13,19 +13,22 @@
           <span>{{ new Date(item.createdAt).toLocaleString() }}</span>
         </template>
         <template v-slot:[`item.actions`]="{ item }">
-          <v-icon button @click="deleteNote(item)">mdi-delete</v-icon>
+          <v-icon button @click="editNote(item)">mdi-pencil</v-icon>
+          <v-icon class="ml-2" button @click="deleteNote(item)"
+            >mdi-delete</v-icon
+          >
         </template>
       </v-data-table>
     </v-card>
     <v-dialog v-model="dialog.active" max-width="500">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn class="mt-2" v-bind="attrs" v-on="on">
+        <v-btn class="mt-2" v-bind="attrs" v-on="on" @click="resetDialog">
           <v-icon class="mr-2">mdi-plus</v-icon>New note
         </v-btn>
       </template>
       <v-card>
         <v-card-title>
-          Create new note
+          {{ dialog.editMode ? "Edit note" : "Create new note" }}
         </v-card-title>
         <v-card-text>
           <v-row>
@@ -77,7 +80,8 @@ export default {
     dialog: {
       active: false,
       noteTitle: "",
-      noteDescription: ""
+      noteDescription: "",
+      editMode: false
     },
     noteHeaders: [
       { text: "Title", value: "title", width: 200 },
@@ -104,19 +108,24 @@ export default {
         // Add it to note data
         noteData.description = this.dialog.noteDescription;
 
-      // Call store action to save note
-      this.$store.dispatch("addNote", noteData).then(
-        () => {
-          this.resetDialog();
-        },
-        // Display error alert if an error occured
-        err => {
-          this.errorAlert = {
-            active: true,
-            message: err
-          };
-        }
-      );
+      if (this.dialog.editMode) {
+        // Handle note edit
+        console.log(noteData);
+      } else {
+        // Call store action to save note
+        this.$store.dispatch("addNote", noteData).then(
+          () => {
+            this.resetDialog();
+          },
+          // Display error alert if an error occured
+          err => {
+            this.errorAlert = {
+              active: true,
+              message: err
+            };
+          }
+        );
+      }
     },
     /**
      * Resets the dialog
@@ -125,7 +134,8 @@ export default {
       this.dialog = {
         active: false,
         noteTitle: "",
-        noteDescription: ""
+        noteDescription: "",
+        editMode: false
       };
       this.resetAlert();
     },
@@ -136,6 +146,14 @@ export default {
       this.errorAlert = {
         active: false,
         message: ""
+      };
+    },
+    editNote(note) {
+      this.dialog = {
+        active: true,
+        noteTitle: note.title,
+        noteDescription: note.description,
+        editMode: true
       };
     },
     deleteNote(note) {
@@ -165,6 +183,7 @@ export default {
         return {
           id: x.id,
           title: x.title,
+          description: x.description,
           createdAt: +new Date(x.createdAt),
           assigned: x.createdBy
         };
