718c6e0086da063f054ebb8c9c1c7148e76e4660
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-11-02 21:18:23 +0100

    Go API: Added basic WebSocket setup
    WebSocket setup:
    - Added the gorilla/websocket package to implement WebSockets on the
    Go server
    - Created a route under /ws which upgrades the http connection to
    a WebSocket connection, by using the Upgrader provided in the package
    - Created WebSocket connection can be used to receive and send messages
    - Implemented a function to always listen for messages from the client
    and echo back the received message after logging it
    
    Router:
    - Created a seperate function to setup routes on mux router

--- go-api/main.go
+++ go-api/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -12,8 +19,57 @@ func get(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"result": "GET method called"}`))
 }
 
+func reader(conn *websocket.Conn) {
+	// Always listen for messages from client
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// Print received message
+		log.Println(string(p))
+
+		// Echo back the received message
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	// Always allow client to upgrade independent of origin
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// Upgrade to websocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected")
+
+	// Greet client
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Listen for client messages
+	reader(ws)
+
+}
+
+// Setup routes
+func setupRoutes(router *mux.Router) {
+	router.HandleFunc("/", get).Methods(http.MethodGet)
+	router.HandleFunc("/ws", wsEndpoint)
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", get).Methods(http.MethodGet)
-	http.ListenAndServe(":8080", r)
+	router := mux.NewRouter()
+	setupRoutes(router)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
