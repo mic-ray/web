b893607f3c55c5abbfba26bfa4a3ca54da523a16
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-11-06 20:38:18 +0100

    JS FE: Established connection to WebSocket endpoint
    HTML:
    - Added connection button and a container holding the message list,
    which is not shown initially
    
    CSS:
    - Moved .css file to seperate folder, to hold all styles files
    - Set main colors to dark theme
    - Applied styling to connect button and message list
    
    JS:
    - Added event handler for connect button onclick, which calls a function
    to connect to the WebSocket endpoint
    - Status of connection is displayed in a dynamic p tag
    - On successful connect socket sends a greet message
    - Received messages are displayed in the toggled message list

--- js-fe/script.js
+++ js-fe/script.js
@@ -1,5 +1,41 @@
-var title = document.getElementById("title");
-var subTitle = document.createElement("h3");
-subTitle.textContent = "This is a subtitle";
+const outputMessage = msg => {
+  document.getElementsByClassName("messageContainer")[0].style.display =
+    "block";
+  var messageList = document.getElementById("messageList");
+  var message = document.createElement("li");
+  message.textContent = msg;
+  messageList.append(message);
+};
 
-title.after(subTitle);
+const connectWS = () => {
+  var socket = new WebSocket("ws://localhost:8080/ws");
+
+  socket.onopen = () => {
+    connectStatus.textContent = "Connected";
+    socket.send("Hi from the Client!");
+  };
+
+  socket.onmessage = message => {
+    outputMessage(message.data);
+  };
+};
+
+const connectHandler = () => {
+  connectBtn.disabled = true;
+
+  // Only add loading info if it
+  if (!document.getElementById("status")) {
+    connectBtn.after(connectStatus);
+    connectStatus.hidden = false;
+  }
+
+  connectWS();
+};
+
+var connectBtn = document.getElementById("connect");
+connectBtn.addEventListener("click", connectHandler);
+
+var connectStatus = document.createElement("p");
+connectStatus.id = "status";
+connectStatus.textContent = "Loading...";
+connectStatus.hidden = true;
