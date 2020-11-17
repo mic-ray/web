94c4114842cb3cfe283c131a0772e58366617bd1
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-11-17 18:12:04 +0100

    Node Gateway: Added missing routes
    Gateway:
    - Added the /users and /notes routes of the Node API to the proxy, which
    can now correctly handle all requests which are supposed to reach the
    intended Node API, by forwarding to the corresponding routes
    - Added example middleware, which is called on each request before
    proxying the route, to log incoming request methods and path

--- node-gateway/index.js
+++ node-gateway/index.js
@@ -2,15 +2,33 @@ const gateway = require("fast-gateway");
 
 const PORT = 3000;
 
-gateway({
-  middlewares: [require("cors")()],
+/**
+ * Middleware for logging incoming requests
+ */
+const logger = (req, res, next) => {
+  console.log(`Received a ${req.method} request on path ${req.path}`);
+  next();
+};
+
+const server = gateway({
+  middlewares: [require("cors")(), logger],
   routes: [
     {
       prefix: "/auth",
       prefixRewrite: "/auth",
       target: "http://localhost:3030"
+    },
+    {
+      prefix: "/users",
+      prefixRewrite: "/users",
+      target: "http://localhost:3030"
+    },
+    {
+      prefix: "/notes",
+      prefixRewrite: "/notes",
+      target: "http://localhost:3030"
     }
   ]
-})
-  .start(PORT)
-  .then(() => console.log("Gateway running on port: ", PORT));
+});
+
+server.start(PORT).then(() => console.log("Gateway running on port:", PORT));
