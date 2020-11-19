8b0e52261ffadc3c346f34ab227aa6739cbd7b52
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-11-19 18:12:23 +0100

    C# API: Added tutorial classes
    TodoItem tutorial:
    - Based on a ASP.NET tutorial added model and database context classes
    for TodoItems
    - Added packages for code generation, which are used to generate the
    controller for handling requests related to TodoItems
    - Contoller handles different HTTP methods of the TodoItems endpoint
    - Removed boilerplate code related to the swagger endpoint

--- c#-api/Startup.cs
+++ c#-api/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using c__api.Models;
 
 namespace c__api
 {
@@ -26,12 +20,8 @@ namespace c__api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "c__api", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +30,6 @@ namespace c__api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "c__api v1"));
             }
 
             app.UseHttpsRedirection();
