a120f952e574f2be37ea7664b97796b8e0e6dd31
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-11-23 23:07:24 +0100

    PyGraph API: Initial commit
    GraphQL API:
    - Created a basic Python API using Flask
    - Added flask-graphql to expose an endpoint, that displays an GraphiQL
    site to try out requests on the schema
    - Used the graphene package to create a simple schema
    - Graph API is seperated from the other Python Flask (SocketIO) folder to
    isolate different technologies

---
+++ py-graph-api/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+import graphene
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+class User(graphene.ObjectType):
+    username = graphene.String()
+    id = graphene.ID()
+
+
+class Query(graphene.ObjectType):
+    users = graphene.List(User)
+
+    def resolve_users(self, info):
+        return [User(username="Mike"), User(username="Mikey"), User(username="Mick")]
+
+
+schema = graphene.Schema(query=Query)
+
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view(
+        "graphql", schema=schema, graphiql=True  # for having the GraphiQL interface
+    ),
+)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
